bd3de031f7145674614567811e3d6800d6418d07
TopScrech <sergei_saliukov@icloud.com>
2026-01-28 09:25:24 +0100

    Initial Commit

---
+++ Git Manager/Git_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_ManagerApp.swift
+//  Git Manager
+//
+//  Created by Sergei Saliukov on 28.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Git_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
